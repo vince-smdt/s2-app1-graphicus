88c98190121d6c644a39366492d5e917c3e9bec9
Kgo20 <89485822+Kgo20@users.noreply.github.com>
2024-01-10 10:46:06 -0500

    main.cpp mode test

--- main.cpp
+++ main.cpp
@@ -3,27 +3,27 @@
 int main() {
     setlocale(LC_ALL, "");
 
-    // Tests tests;
-    // tests.tests_unitaires();
+    Tests tests;
+    tests.tests_unitaires();
 
-    Canevas c;
+    // Canevas c;
 
-    c.activerCouche(0);
-    c.ajouterForme(new Rectangle(2, 3, 1, 1));
-    c.ajouterForme(new Carre(0, 0, 3));
-    c.ajouterForme(new Cercle(7, 8, 6));
+    // c.activerCouche(0);
+    // c.ajouterForme(new Rectangle(2, 3, 1, 1));
+    // c.ajouterForme(new Carre(0, 0, 3));
+    // c.ajouterForme(new Cercle(7, 8, 6));
 
-    c.activerCouche(2);
-    c.ajouterForme(new Rectangle(5, 6, 3, 4));
-    c.ajouterForme(new Carre(3, 4, 1));
-    c.ajouterForme(new Cercle(0, 0, 3));
+    // c.activerCouche(2);
+    // c.ajouterForme(new Rectangle(5, 6, 3, 4));
+    // c.ajouterForme(new Carre(3, 4, 1));
+    // c.ajouterForme(new Cercle(0, 0, 3));
 
-    c.activerCouche(3);
-    c.ajouterForme(new Cercle(-3, 6, 1));
+    // c.activerCouche(3);
+    // c.ajouterForme(new Cercle(-3, 6, 1));
 
-    c.activerCouche(0);
+    // c.activerCouche(0);
 
-    c.afficher(cout);
+    // c.afficher(cout);
 
-    return cin.get();
+    // return cin.get();
 }
